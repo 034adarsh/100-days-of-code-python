3115e50ec8af096af626f2323d638c4c94d43dfd
Adarsh Kumar Singh <95336274+034adarsh@users.noreply.github.com>
2023-03-06 23:43:51 +0530

    Update Sum_of_even.py

--- Sum_of_even.py
+++ Sum_of_even.py
@@ -1,1 +1,11 @@
+#Write your code below this row 👇
+n = int(input("Enter a number (till where you want to add even numbers): "))
+sum=0
+for i in range(n):
+  if i%2==0:
+    sum = sum + i
+  else:
+    continue
+print(sum)
+    
 
