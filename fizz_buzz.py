ecd3d20052beb92f3800ed309fb79eea635ad55a
Adarsh Kumar Singh <95336274+034adarsh@users.noreply.github.com>
2023-03-07 23:21:01 +0530

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,14 @@
-
+#Write your code below this row 👇
+for i in range(1, 101):
+    if i%3==0:
+        if i%5==0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
+    elif i%5==0:
+        if i%3==0:
+            print("FizzBuzz")
+        else:
+            print("Buzz")
+    else:
+        print(i)
